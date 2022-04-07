f8ea502b8dd74b9f444787bbf4977d028e310c55
Kevin Benelli <kevinbenelli@Kevins-MacBook-Pro.local>
2022-04-06 17:25:20 -0700

    update package

--- server/socket-server.js
+++ server/socket-server.js
@@ -188,3 +188,13 @@ io.on("connection", (socket) => {
 server.listen(port, () => {
   console.log(`Listening at http://localhost:${port}`);
 });
+
+const path = require("path");
+
+// Serve static files from the React frontend app
+server.use(express.static(path.join(__dirname, "../client/build")));
+
+// AFTER defining routes: Anything that doesn't match what's above, send back index.html; (the beginning slash ('/') in the string is important!)
+server.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/../client/build/index.html"));
+});
